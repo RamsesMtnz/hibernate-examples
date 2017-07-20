9118c596b482c26f43af3e992985316835661b13
conrado <conrado@conrado-pc>
2017-07-20 09:23:44 -0500

    contexto de persistencia

--- src/main/java/com/github/conradovera/annotationmapping/MainRamses.java
+++ src/main/java/com/github/conradovera/annotationmapping/MainRamses.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -14,61 +15,82 @@ public class MainRamses {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-				.configure("hibernateAnotationsRamses.cfg.xml") // configures settings from hibernate.cfg.xml
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernateAnotationsRamses.cfg.xml") // configures
+																															// settings
+																															// from
+																															// hibernate.cfg.xml
 				.build();
-		
+
 		SessionFactory sessionFactory = null;
-		
+
 		try {
-			sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-		}
-		catch (Exception e) {
-			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		} catch (Exception e) {
+			// The registry would be destroyed by the SessionFactory, but we had
+			// trouble building the SessionFactory
 			// so destroy it manually.
 			e.printStackTrace();
-			StandardServiceRegistryBuilder.destroy( registry );
+			StandardServiceRegistryBuilder.destroy(registry);
 		}
-		
+
 		Session session = sessionFactory.openSession();
-                //MUCHO OJO AQUI, EN LA SIGUIENTE LINEA EMPIEZA UNA TRANSACCION
-		session.beginTransaction();
-		session.save( new EventRamses( "Our very first event!", new Date(),1L) );
-		session.save( new EventRamses( "A follow up event", new Date() ,2L));
-		session.save( new LugarRamses( "Vamos al cine", "Plaza Galerias Campeche" ) );
-		session.save( new LugarRamses( "Vamo a comer", "Boston" ) );
-                //MUCHO OJO AQUI, EN LA SIGUIENTE LINEA FINALIZA LA TRANSACCION
-		session.getTransaction().commit();
+
+		//Rescato la transaccion ya creada previamente por JDBC
+		Transaction transaccion = session.getTransaction();
+
+		//Empiezo mi proceso transaccional
+		transaccion.begin();
+		session.save(new EventRamses("Our very first event!", new Date(), 1L));
+		session.save(new EventRamses("A follow up event", new Date(), 2L));
+		session.save(new LugarRamses("Vamos al cine", "Plaza Galerias Campeche"));
+		session.save(new LugarRamses("Vamo a comer", "Boston"));
+		//Aplicar cambios en BD
+		transaccion.commit();
 		
-		//System.out.println(session.get(Event.class, 1L).getTitle());
-		
-		List<EventRamses> result = session.createQuery( "from EventRamses" ).list();
-		for ( EventRamses event : result ) {
-			System.out.println( "Event (" + event.getDate() + ") : " + event.getTitle() );
+		List<EventRamses> result = session.createQuery("from EventRamses").list();
+		for (EventRamses event : result) {
+			System.out.println("Event (" + event.getDate() + ") : " + event.getTitle());
 		}
-                
-                List<LugarRamses> result2 = session.createQuery( "from LugarRamses" ).list();
-		for ( LugarRamses event2 : result2 ) {
-			System.out.println( "Hoy " + event2.getNombre() + " pero en" + event2.getCiudad() );
+
+		System.out.println("Pido los lugares por primera vez");
+		List<LugarRamses> result2 = session.createQuery("from LugarRamses").list();
+		for (LugarRamses event2 : result2) {
+			System.out.println("Hoy " + event2.getNombre() + " pero en" + event2.getCiudad());
 		}
 		
-                
-                LugarRamses lugarModificar = session.get(LugarRamses.class, 2L);
-                lugarModificar.setNombre("Vamos a comer");
-                //UPDATE
-                //MUCHO OJO AQUI, EN LA SIGUIENTE LINEA EMPIEZA UNA TRANSACCION
-		session.beginTransaction();
-                
-                
-                
-                
-                //MUCHO OJO AQUI, EN LA SIGUIENTE LINEA FINALIZA LA TRANSACCION
-		session.getTransaction().commit();
-                
+		//Asi pido el registro entero con datos
+		//LugarRamses lugarModificar = session.get(LugarRamses.class, 2L);
+		
+		//Asi pido unicamente la referencia del registro
+		LugarRamses lugarModificar = session.load(LugarRamses.class, 2L);
+		
+		transaccion.begin();
+		lugarModificar.setNombre("Vamos a comer");
+		
+		//Pregunto si la entidad esta en el contexto de persistencia
+		//System.out.println("¿Estas en el contexto de persistencia? "+session.contains(lugarModificar));
+		
+		//Saco la entidad del contexto de persistencia para trabajar con ella en la vista por ejemplo
+		//session.detach(lugarModificar);
+		
+		//Pregunto si la entidad esta en el contexto de persistencia
+		//System.out.println("¿Estas en el contexto de persistencia? "+session.contains(lugarModificar));
+		
+		//Regreso la entidad sacada previamente al contexto de persistencia
+		//session.merge(lugarModificar);
+		transaccion.commit();
+		
+
+		System.out.println("Pido los lugares por segunda vez");
+		List<LugarRamses> lugares = session.createQuery("from LugarRamses").list();
+		for (LugarRamses event2 : lugares) {
+			System.out.println("Hoy " + event2.getNombre() + " pero en" + event2.getCiudad());
+		}
+		
 		session.close();
-		
+
 		sessionFactory.close();
-		
+
 	}
 
 }
